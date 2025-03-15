2347aa19cebfa9f43758777f297e5b28b7d1f6b3
Giovani Finazzi <53719063+GiovaniFZ@users.noreply.github.com>
2025-03-15 12:30:41 -0300

    ref: Indent Aula45.c

--- Aula45.c
+++ Aula45.c
@@ -1,33 +1,26 @@
 #include <stdio.h>
 
 struct horario{
-
-int horas;
-int minutos;
-int segundos;
-
+	int horas;
+	int minutos;
+	int segundos;
 };
 
 int main(){
-struct horario receber(struct horario agora[]);
-struct horario agora[5];
+	struct horario receber(struct horario agora[]);
+	struct horario agora[5];
 
-printf("Digite 5 horarios no formato hh:mm:ss\n");
+	printf("Digite 5 horarios no formato hh:mm:ss\n");
 
-for(int i = 0; i < 5; ++i){
-scanf("%i %i %i", &agora[i].horas, &agora[i].minutos, &agora[i].segundos);
-}
-receber(agora);
+	for(int i = 0; i < 5; ++i){
+		scanf("%i %i %i", &agora[i].horas, &agora[i].minutos, &agora[i].segundos);
+	}
+	receber(agora);
 }
 
 struct horario receber(struct horario agora[]){
-
-printf("\nOs horarios digitados foram:\n ");
-
-for(int i = 0; i < 5; ++i){
-
-printf("horario %i = %i : %i : %i\n", i + 1, agora[i].horas, agora[i].minutos, agora[i].segundos);
-
-}
-
+	printf("\nOs horarios digitados foram:\n ");
+	for(int i = 0; i < 5; ++i){
+		printf("horario %i = %i : %i : %i\n", i + 1, agora[i].horas, agora[i].minutos, agora[i].segundos);
+	}
 }
