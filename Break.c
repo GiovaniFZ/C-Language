d6857b89f2f996d59cba5bfd1d4c2a6f751000cc
Giovani Finazzi <53719063+GiovaniFZ@users.noreply.github.com>
2025-03-17 21:03:05 -0300

    Indent Break.c

--- Break.c
+++ Break.c
@@ -2,17 +2,14 @@
 
 int main(){
 
-for(int i = 1; i < 30; ++i){
+  for(int i = 1; i < 30; ++i){
+    if(i % 10 == 0){
+      printf("O numero da variavel aqui eh 10\n");
+      break;
+    } else {
 
-if(i % 10 == 0){
-printf("O numero da variavel aqui eh 10\n");
-break;
-
-}else{
-
-printf("%i nao eh 10\n", i);
-
-}
-}
-getchar();
+    printf("%i nao eh 10\n", i);
+    }
+  }
+  getchar();
 }
