513a4b8ec36463ac9c224093d139c3b9bd808e54
Giovani Finazzi <53719063+GiovaniFZ@users.noreply.github.com>
2025-03-28 17:56:24 -0300

    Indent Exercicio.c

--- Divisao e resto/Exercicio.c
+++ Divisao e resto/Exercicio.c
@@ -1,25 +1,18 @@
 #include <stdio.h>
 
-int main (){
+int main () {
+  int Num1, Num2;
+  printf("Digite um numero inteiro\n");
+  scanf("%i", &Num1);
+  printf("Digite um outro numero inteiro\n");
+  scanf("%i", &Num2);
+  if(Num1 % Num2 == 0) {
+    printf("O primeiro numero digitado eh divisivel pelo segundo\n");
+  }
+  else {
+    printf("O primeiro numero digitado NAO eh divisivel pelo segundo.\n");
+  }
 
-int Num1, Num2;
-printf("Digite um numero inteiro\n");
-scanf("%i", &Num1);
-printf("Digite um outro numero inteiro\n");
-scanf("%i", &Num2);
-if(Num1 % Num2 == 0){
-
-printf("O primeiro numero digitado eh divisivel pelo segundo\n");
-
-}else {
-
-printf("O primeiro numero digitado NAO eh divisivel pelo segundo.\n");
-
-
-}
-
-return 0;
-getchar();
-
-
+  return 0;
+  getchar();
 }
