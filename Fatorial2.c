55c163f3678c69c9544e72da0454a4c887b0b865
Giovani Finazzi <53719063+GiovaniFZ@users.noreply.github.com>
2025-06-10 14:32:30 -0300

    Ident Fatorial2.c

--- Fatorial2.c
+++ Fatorial2.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
 
-int main(){
+int main() {
+  int numero = 0;
+  int fatorial = 1;
+  printf("Digite um numero: ");
+  scanf("%i", &numero);
 
-int numero = 0;
-int fatorial = 1;
-printf("Digite um numero: ");
-scanf("%i", &numero);
-
-for(int i = numero; i > 1; i = i - 1){
-
-fatorial *= i;
-
-}
-
-printf("O fatorial eh %i\n", fatorial);
-
+  for(int i = numero; i > 1; i = i - 1){
+    fatorial *= i;
+  }
+  printf("O fatorial eh %i\n", fatorial);
 }
