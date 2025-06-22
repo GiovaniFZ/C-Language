20c027b6d79dc3f7f9328bb126deef31160d75a8
Giovani Finazzi <53719063+GiovaniFZ@users.noreply.github.com>
2025-06-22 15:52:16 -0300

    Indent Fatorial3.c

--- Fatorial3.c
+++ Fatorial3.c
@@ -1,43 +1,40 @@
 #include <stdio.h>
 
-int main(){
+int main() {
 
-int resultado;
-int numero;
-int i = 0;
-int CalcularFatorial(int num1);
+  int resultado;
+  int numero;
+  int i = 0;
+  int CalcularFatorial(int num1);
 
-printf("Digite um numero inteiro: ");
-scanf("%i", &numero);
+  printf("Digite um numero inteiro: ");
+  scanf("%i", &numero);
 
-resultado = CalcularFatorial(numero);
-printf("O Fatorial do numero digitado eh %i\n", resultado);
+  resultado = CalcularFatorial(numero);
+  printf("O Fatorial do numero digitado eh %i\n", resultado);
 
 }
 
-int CalcularFatorial(int num1){
-int res = 1;
-int i = 0;
+int CalcularFatorial(int num1)  {
+  int res = 1;
+  int i = 0;
 
-i = num1;
+  i = num1;
 
-if(i == 0){
-
-res = 1;
-
-}else{
-
-if(i > 1){
-while(i > 1){
-
-res *= i;
---i;
-}
-return res;
-}else{
-
-printf("O valor digitado nao eh inteiro\n");
-return 0;
-}
-}
+  if(i == 0) {
+    res = 1;
+  }
+  else {
+    if(i > 1) {
+      while(i > 1) {
+        res *= i;
+        --i;
+      }
+    return res;
+    }
+    else {
+      printf("O valor digitado nao eh inteiro\n");
+      return 0;
+    }
+  }
 }
