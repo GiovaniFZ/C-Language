7e40f0862e02c725ef23000d0c51a3faa62db250
GiovaniFZ <giovanifinazzi@gmail.com>
2026-04-30 19:16:27 -0300

    refactor: indent concat strings

--- Concatenação.c
+++ Concatenação.c
@@ -1,35 +1,31 @@
 #include <stdio.h>
 
-int main(){
+int main() {
 
-void ConcatenarStrings(char c[], char d[], char New[]);
+    void ConcatenarStrings(char c[], char d[], char New[]);
 
-char a[8] = {'L', 'i', 'n', 'e', 'a', 'g', 'e', ' '};
-char b[4] = {'t', 'r', 'e', 'e'};
-char NovaPalavra[12];
+    char a[8] = {'L', 'i', 'n', 'e', 'a', 'g', 'e', ' '};
+    char b[4] = {'t', 'r', 'e', 'e'};
+    char NovaPalavra[12];
  
 
-ConcatenarStrings(a, b, NovaPalavra);
+    ConcatenarStrings(a, b, NovaPalavra);
 
-for(int i = 0; i < 12; i++){
-printf("%c", NovaPalavra[i]);
+    for(int i = 0; i < 12; i++) {
+        printf("%c", NovaPalavra[i]);
+    }
 
-}
-printf("\n");
-}
-void ConcatenarStrings(char c[], char d[], char New[]){
-
-for(int i = 0; i < 8; i++){
-
-New[i] = c[i];
-
+    printf("\n");
 }
 
-for(int j = 0; j < 4; j++){
+void ConcatenarStrings(char c[], char d[], char New[]) {
 
-New[8 + j] = d[j];
+    for(int i = 0; i < 8; i++) {
+        New[i] = c[i];
+    }
 
-
-}
+    for(int j = 0; j < 4; j++) {
+        New[8 + j] = d[j];
+    }
 
 }
